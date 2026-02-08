1b87283362296609d4ecf2a8b752015cfeebc95f
sanka <alexsandrvladimirovih2008@gmail.com>
2026-02-08 19:35:53 +0500

    какая-то дристня, у меня все время вылазит not found, при нажатии на UserItem, хз как это фиксить, я чет устал, досвидание

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import Card, { CardVariant } from './components/Card';
-import UserList from './components/UserList';
-import {IUser} from './components/types/types'
-
+import {BrowserRouter, Link, Routes, Route} from "react-router-dom";
+import UsersPage from "./components/UsersPage";
+import TodosPage from "./components/TodosPage";
+import UserItemPage from "./components/UserItemPage";
+import TodoItemPage from "./components/TodoItemPage";
 const App = () => {
-  const users: IUser[] = [
-    {id: 1, name: 'Sanya', email: 'alexsandrvladimirovih2008@gmail.com', address: {city: 'Ekb', street: 'Saharova 69', zipcode: 'chestno-hz'}},
-    {id: 2, name: 'Zheka', email: 'brownbich@gmail.com', address: {city: 'Tomsk', street: 'Lebedeva 38', zipcode: 'chestno-hz'}}
-  ]
   return(
     <div>
-      <Card onClick = {(num) => console.log('click', num)} variant = {CardVariant.primary} width='200px' height='200px'>
-        <button>Кнопка</button>
-      </Card>
-        <UserList users = {users}/>
+        <BrowserRouter>
+            <div>
+                <div>
+                    <Link to="/users">Пользователи</Link>
+                    <Link to="/todos">Список дел</Link>
+                </div>
+                <Routes>
+                    <Route path="/users" element={<UsersPage/>}/>
+                    <Route path="/todos" element={<TodosPage/>}/>
+                    <Route path="/user/:id" element={<UserItemPage/>}/>
+                    <Route path="/todo/:id" element={<TodoItemPage/>}/>
+                    <Route path="*" element={<div>NotFound</div>}/>
+                </Routes>
+            </div>
+        </BrowserRouter>
     </div>
   );
 };
